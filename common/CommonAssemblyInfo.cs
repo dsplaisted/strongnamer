30b25d0f7ad223a39d966c0bde43170c09441521
Daniel Plaisted <dsplaisted@gmail.com>
2016-08-22 15:01:26 -0700

    Update package version to 0.0.2

--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -26,5 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+
+//  No need to manually update this, build process does it automatically
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
